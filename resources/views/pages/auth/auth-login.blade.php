b7fe35eb26a80f81ee75a509968df17ff9de1e6b
AgungR12 <2000agungramadhan@gmail.com>
2024-02-16 14:40:11 +0700

    update terbaru

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -73,7 +73,7 @@
         </div>
     </div>
     <div class="text-muted mt-5 text-center">
-        Don't have an account? <a href="{{ route('register') }}">Create One</a>
+        Don't have an account? <a href="{{ route('register') }}">Registrasi</a>
     </div>
 @endsection
 
